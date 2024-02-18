00cf5daabe9d27bddecf62b78e0ddfe7f451c163
Kris Kurzawa <kurzawa1@yahoo.com>
2024-02-18 15:44:27 -0500

    css music

--- src/Pages/Music/Music.jsx
+++ src/Pages/Music/Music.jsx
@@ -1,18 +1,23 @@
 import './Music.css'
 import AudioPlayer from '../../Components/AudioPlayer/AudioPlayer'
+import Sly from '../../assets/images/Sly.jpg';
+import VisionQuest from '../../assets/images/VisionQuest.jpeg'
+import ZapToro from '../../assets/images/ZapToro.jpeg'
 
 const Music = () => {
     return (
-        <div className='flex flex-col justify-center items-center'>
-            <header className='text-[whitesmoke] text-center text-2xl'>Listen</header>
-            <main className='mainMusic'>
-
-                <article className='w-full'>
+        <main className='flex flex-col justify-center items-center'>
+            <header className='text-[whitesmoke] text-center text-3xl py-3'>Listen</header>
+            <article className='mainMusic'>
+                <section className='w-full'>
                     <AudioPlayer />
-                </article>
-
-            </main>
-        </div>
+                </section>
+            </article>
+            <header className='text-[whitesmoke] text-center text-3xl py-3'>Download</header>
+            <a href='https://www.amazon.com/Sly-Kris-Kurzawa/dp/B01AXGA21W' target="_blank" rel="noreferrer"><img src={Sly} className='w-[325px] h-auto py-4' /></a>
+            <a href='https://www.amazon.com/Zap-Toro/dp/B004P91DSU' target="_blank" rel="noreferrer"><img src={ZapToro} className='w-[325px] h-auto py-4' /></a>
+            <a href='https://www.amazon.com/Vision-Quest-Zap-Toro/dp/B01M9CIBPC' target="_blank" rel="noreferrer"><img src={VisionQuest} className='w-[325px] h-auto py-4 mb-4' /></a>
+        </main>
 
     )
 }
