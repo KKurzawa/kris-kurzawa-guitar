978958cca46150cee9e6654649f0e03e8531d0cb
Kris Kurzawa <kurzawa1@yahoo.com>
2024-02-22 12:55:00 -0500

    updated shows

--- src/Pages/Shows/Shows.jsx
+++ src/Pages/Shows/Shows.jsx
@@ -1,9 +1,51 @@
 import './Shows.css'
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import { ShowsList } from '../../assets/data/showsList';
 
 const Shows = () => {
+    function notYetAvailable() {
+        alert("Either tickets are not yet available for purchase or they will be made available at the door.  Please check back later or contact the venue.")
+    }
+
+    function noCover() {
+        alert("This is a free show!")
+    }
+
+    function reservations() {
+        alert("You must contact the venue directly for reservations.")
+    }
+
     return (
-        <div>Shows</div>
+        <main>
+            {ShowsList.map((item) => (
+                <ol key={item.index} className='flex flex-col items-center text-[whitesmoke] text-2xl'>
+                    <section className='flex flex-row'>
+                        <li className=''>{item.date}</li>
+                        <li className=''>{item.time}</li>
+                    </section>
+                    <section className='flex flex-row'>
+                        <li className=''>{item.band}</li>
+                        <li>@</li>
+                        <a href={item.venueLink} target="_blank" rel="noreferrer"><li className=''>{item.venue}</li></a>
+                    </section>
+                    <li>
+                        {item.ticketLink === false ? (
+                            <button onClick={notYetAvailable} className='ticket-btn '>Tickets</button>
+                        ) :
+                            item.ticketLink === 1 ? (
+                                <button onClick={noCover}
+                                    className='ticket-btn'>Tickets</button>
+                            ) :
+                                item.ticketLink === 2 ? (
+                                    <button onClick={reservations} className='ticket-btn'>Tickets</button>
+                                ) :
+                                    (
+                                        <a href={item.ticketLink} target="_blank" rel="noreferrer" ><button className='ticket-btn'>Tickets</button></a>
+                                    )}
+                    </li>
+                </ol>
+            ))}
+        </main>
     )
 }
 
---
+++ src/assets/data/showsList.jsx
@@ -0,0 +1,82 @@
+export const ShowsList = [
+    {
+        date: "February 24 2024",
+        time: '7pm',
+        band: 'Caravan Gypsy',
+        venue: 'The Detroit Club',
+        venueLink: 'https://www.thedetroitclub.com/',
+        ticketLink: 1
+    },
+    {
+        date: 'March 2 2024',
+        time: '8:30pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        venue: 'The Cadieux Cafe',
+        venueLink: 'https://www.cadieuxcafe.com',
+        ticketLink: "https://www.eventbrite.com/e/revival-a-tribute-to-the-allman-brothers-tickets-809714276197?aff=oddtdtcreator&fbclid=IwAR1uOMXJs_xYGMmSYmaqiuYAmLUR4sAu92R_c2FCuViLj7zzUZLa24uTY74"
+    },
+    {
+        date: 'March 14 2024',
+        time: '7pm',
+        band: 'Caravan Gypsy',
+        venue: "Cata Vino",
+        venueLink: "https://laferiadetroit.com/",
+        ticketLink: 1
+    },
+    {
+        date: 'April 11 2024',
+        time: '7pm',
+        band: 'Caravan Gypsy',
+        venue: "Cata Vino",
+        venueLink: "https://laferiadetroit.com/",
+        ticketLink: 1
+    },
+    {
+        date: 'April 27 2024',
+        time: '8:30pm',
+        band: 'The Kris Kurzawa Group Plays Steely Dan',
+        venue: 'The Cadieux Cafe',
+        venueLink: 'https://www.cadieuxcafe.com',
+        ticketLink: false
+    },
+    {
+        date: 'November 7 2024',
+        time: '6pm',
+        band: 'The Kris Kurzawa Group',
+        venue: 'The Dirty Dog Jazz Cafe',
+        venueLink: 'https://dirtydogjazz.com/',
+        ticketLink: 2
+    },
+    {
+        date: 'November 8 2024',
+        time: '6pm',
+        band: 'The Kris Kurzawa Group',
+        venue: 'The Dirty Dog Jazz Cafe',
+        venueLink: 'https://dirtydogjazz.com/',
+        ticketLink: 2
+    },
+    {
+        date: 'November 8 2024',
+        time: '8:30pm',
+        band: 'The Kris Kurzawa Group',
+        venue: 'The Dirty Dog Jazz Cafe',
+        venueLink: 'https://dirtydogjazz.com/',
+        ticketLink: 2
+    },
+    {
+        date: 'November 9 2024',
+        time: '6pm',
+        band: 'The Kris Kurzawa Group',
+        venue: 'The Dirty Dog Jazz Cafe',
+        venueLink: 'https://dirtydogjazz.com/',
+        ticketLink: 2
+    },
+    {
+        date: 'November 9 2024',
+        time: '8:30pm',
+        band: 'The Kris Kurzawa Group',
+        venue: 'The Dirty Dog Jazz Cafe',
+        venueLink: 'https://dirtydogjazz.com/',
+        ticketLink: 2
+    },
+]
