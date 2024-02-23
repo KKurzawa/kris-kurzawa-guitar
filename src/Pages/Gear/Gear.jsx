6aa1f42a0386007a431b3bba37ac71ae64c3b64a
Kris Kurzawa <kurzawa1@yahoo.com>
2024-02-23 18:46:25 -0500

    updated gear

--- src/Pages/Gear/Gear.jsx
+++ src/Pages/Gear/Gear.jsx
@@ -3,6 +3,30 @@ import PedalBoard from '../../assets/images/PedalBoard.jpg'
 import Guitar3 from '../../assets/images/Guitar3.jpg'
 import { LazyLoadImage } from "react-lazy-load-image-component";
 
+const awards = [
+    { title: '2023 Outstanding Tribute Band' },
+    { title: '2017 Outstanding Jazz Composer' },
+    { title: '2016 Outstanding Jazz Recording' },
+    { title: '2016 Outstanding Modern Jazz Artist' },
+    { title: '2015 Outstanding World Artist' },
+    { title: '2015 Outstanding World Songwriter' },
+    { title: '2011 Outstanding World Songwriter' }
+]
+
+const nominations = [
+    { title: '2015 Outstanding Jazz Composer' },
+    { title: '2015 Outstanding Jazz Instrumentalist' },
+    { title: '2015 Outstanding World Instrumentalist' },
+    { title: '2015 Outstanding Jazz Composer' },
+    { title: '2012 Outstanding Jazz Instrumentalist' },
+    { title: '2012 Outstanding World Instrumentalist' },
+    { title: '2012 Outstanding World Songwriter' },
+    { title: '2011 Outstanding World Instrumentalist' },
+    { title: '2011 Outstanding Urban Musician' },
+    { title: '2010 Outstanding Urban Musician' },
+    { title: '2009 Outstanding Urban Musician' },
+]
+
 const meatPot = [
     {
         id: 1,
@@ -80,26 +104,54 @@ const gravy = [
 ]
 const Gear = () => {
     return (
-        <main className='text-[whitesmoke] flex flex-col lg:flex-row lg:justify-evenly items-center min-h-full lg:mb-5 py-5 '>
-            <article className='gearContainer flex flex-col mb-5 lg:mb-0 lg:-mt-[6.65rem]'>
-                <h2 className='text-3xl font-bold pb-2'>Meat & Potatoes</h2>
-                <LazyLoadImage src={Guitar3} className='gear-image w-96 lg:max-w-full' />
-                <ol className='gear-img mt-2'>
-                    {meatPot.map((meat) => (
-                        <a key={meat.id} href={meat.link} target="_blank" rel="noreferrer" className='my-[.15rem]'><li className='text-xl'>{meat.name}</li></a>
-                    ))}
-                </ol>
+        <main className='text-[whitesmoke] flex flex-col lg:mb-5 py-5 '>
+            <article className='flex flex-col items-center'>
+                <header className='text-4xl font-bold'>Bio</header>
+                <section className='section1 lg:mx-32 mx-3 mt-3 mb-5 p-4 lg:p-10 text-center'>
+                    <h2 className=' text-md lg:text-2xl mx-5'>Kris Kurzawa’s versatility has led him to become one of Detroit area’s most in demand guitarists
+                        since the early 2000’s; crossing multiple genres including jazz, fusion, rock, world music and
+                        R+B. He released his first album ‘Sly’ in 2016 earning him his fifth Detroit Music Award. He
+                        earned his bachelors degree in 2003 and his masters degree in 2012 with a concentration in
+                        jazz studies from Wayne State University. Kris has over 20 years teaching experience and is
+                        the head of the guitar department at the Detroit Institute of Music Education and adjunct faculty
+                        in Wayne State Universities jazz studies program.</h2>
+                </section>
+                <section className='section2 p-4 lg:p-10 my-5 text-center'>
+                    <a href='https://www.detroitmusicawards.net/' target="_blank" rel="noreferrer" className='text-3xl lg:text-4xl mb-2 font-bold'>Detroit Music Awards</a>
+                    <ul className='mb-5'>
+                        <header className="text-2xl lg:text-3xl mb-1 font-bold">Awards</header>
+                        {awards.map((award) => (
+                            <li className='text-lg lg:text-2xl' key={award.index}>{award.title}</li>
+                        ))}
+                    </ul>
+                    <ul className='mb-5'>
+                        <header className="text-2xl lg:text-3xl mb-1 font-bold">Nominations</header>
+                        {nominations.map((nomination) => (
+                            <li className='text-lg lg:text-2xl' key={nomination.index}>{nomination.title}</li>
+                        ))}
+                    </ul>
+                </section>
             </article>
-            <article className='gearContainer flex flex-col '>
-                <h2 className='text-3xl font-bold pb-2'>Gravy</h2>
-                <LazyLoadImage src={PedalBoard} className='gear-image w-96 lg:max-w-92' />
-                <ol className='gear-img mt-2'>
-                    {gravy.map((meat) => (
-                        <a key={meat.id} href={meat.link} target="_blank" rel="noreferrer" className='my-[.15rem]'><li className='text-xl'>{meat.name}</li></a>
-                    ))}
-                </ol>
-
-            </article>
+            <section className='flex flex-col lg:flex-row justify-evenly'>
+                <article className='gearContainer flex flex-col mb-5 lg:mb-0 '>
+                    <h2 className='text-3xl font-bold pb-2'>Meat & Potatoes</h2>
+                    <LazyLoadImage src={Guitar3} className='gear-image w-96 lg:max-w-full' />
+                    <ol className='gear-img mt-2'>
+                        {meatPot.map((meat) => (
+                            <a key={meat.id} href={meat.link} target="_blank" rel="noreferrer" className='my-[.15rem]'><li className='text-xl'>{meat.name}</li></a>
+                        ))}
+                    </ol>
+                </article>
+                <article className='gearContainer flex flex-col '>
+                    <h2 className='text-3xl font-bold pb-2'>Gravy</h2>
+                    <LazyLoadImage src={PedalBoard} className='gear-image w-96 lg:max-w-92' />
+                    <ol className='gear-img mt-2'>
+                        {gravy.map((meat) => (
+                            <a key={meat.id} href={meat.link} target="_blank" rel="noreferrer" className='my-[.15rem]'><li className='text-xl'>{meat.name}</li></a>
+                        ))}
+                    </ol>
+                </article>
+            </section>
         </main>
     )
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,30 +5,6 @@ import Sly from '../../assets/images/Sly.jpg';
 import SingleShowCard from '../../Components/SingleShowCard/SingleShowCard';
 import { videoLinks } from '../../assets/data/videoLinks';
 
-const awards = [
-    { title: '2023 Outstanding Tribute Band' },
-    { title: '2017 Outstanding Jazz Composer' },
-    { title: '2016 Outstanding Jazz Recording' },
-    { title: '2016 Outstanding Modern Jazz Artist' },
-    { title: '2015 Outstanding World Artist' },
-    { title: '2015 Outstanding World Songwriter' },
-    { title: '2011 Outstanding World Songwriter' }
-]
-
-const nominations = [
-    { title: '2015 Outstanding Jazz Composer' },
-    { title: '2015 Outstanding Jazz Instrumentalist' },
-    { title: '2015 Outstanding World Instrumentalist' },
-    { title: '2015 Outstanding Jazz Composer' },
-    { title: '2012 Outstanding Jazz Instrumentalist' },
-    { title: '2012 Outstanding World Instrumentalist' },
-    { title: '2012 Outstanding World Songwriter' },
-    { title: '2011 Outstanding World Instrumentalist' },
-    { title: '2011 Outstanding Urban Musician' },
-    { title: '2010 Outstanding Urban Musician' },
-    { title: '2009 Outstanding Urban Musician' },
-]
-
 const Home = () => {
     const singleVideoLink = videoLinks[0].link;
     const singleVideoTitle = videoLinks[0].title;
@@ -38,36 +14,18 @@ const Home = () => {
             <LazyLoadImage src={KrisKurzawaGuitar} className='guitarPic w-full' />
             <article className='flex flex-col items-center'>
                 <header className='text-[whitesmoke] text-3xl mt-5 font-bold'>Download</header>
-                <a href='https://www.amazon.com/Sly-Kris-Kurzawa/dp/B01AXGA21W' target="_blank" rel="noreferrer"><LazyLoadImage src={Sly} className=' sly w-[23rem] lg:w-[50rem] h-auto my-4' /></a>
-                <article className='text-[whitesmoke] flex flex-col py-5'>
+                <a href='https://www.amazon.com/Sly-Kris-Kurzawa/dp/B01AXGA21W' target="_blank" rel="noreferrer"><LazyLoadImage src={Sly} className=' sly w-[23rem] lg:w-[50rem] h-auto mt-1 mb-4' /></a>
+                {/* large */}
+                <article className='text-[whitesmoke] hidden lg:flex flex-col py-5'>
                     <header className='pb-1 text-center text-3xl font-bold'>{singleVideoTitle}</header>
                     <iframe className='singleVideo' width="750" height="421.875" src={singleVideoLink} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                 </article>
+                {/* small and medium */}
+                <article className='text-[whitesmoke] lg:hidden flex flex-col py-5'>
+                    <header className='pb-1 text-center text-3xl font-bold'>{singleVideoTitle}</header>
+                    <iframe className='singleVideo' width="350" height="196.875" src={singleVideoLink} title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </article>
                 <SingleShowCard />
-                <section className='section1 lg:mx-32 mx-3 my-5 p-4 lg:p-10 text-center'>
-                    <h2 className=' text-md lg:text-2xl mx-5'>Kris Kurzawa’s versatility has led him to become one of Detroit area’s most in demand guitarists
-                        since the early 2000’s; crossing multiple genres including jazz, fusion, rock, world music and
-                        R+B. He released his first album ‘Sly’ in 2016 earning him his fifth Detroit Music Award. He
-                        earned his bachelors degree in 2003 and his masters degree in 2012 with a concentration in
-                        jazz studies from Wayne State University. Kris has over 20 years teaching experience and is
-                        the head of the guitar department at the Detroit Institute of Music Education and adjunct faculty
-                        in Wayne State Universities jazz studies program.</h2>
-                </section>
-                <section className='section2 p-4 lg:p-10 my-5 text-center'>
-                    <a href='https://www.detroitmusicawards.net/' target="_blank" rel="noreferrer" className='text-3xl lg:text-4xl mb-2 font-bold'>Detroit Music Awards</a>
-                    <ul className='mb-5'>
-                        <header className="text-2xl lg:text-3xl mb-1 font-bold">Awards</header>
-                        {awards.map((award) => (
-                            <li className='text-lg lg:text-2xl' key={award.index}>{award.title}</li>
-                        ))}
-                    </ul>
-                    <ul className='mb-5'>
-                        <header className="text-2xl lg:text-3xl mb-1 font-bold">Nominations</header>
-                        {nominations.map((nomination) => (
-                            <li className='text-lg lg:text-2xl' key={nomination.index}>{nomination.title}</li>
-                        ))}
-                    </ul>
-                </section>
             </article>
         </main>
     )
