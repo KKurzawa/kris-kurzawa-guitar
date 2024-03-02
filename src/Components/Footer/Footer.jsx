9eb6e5fd8720e04502ee64382bed91d5fa32a249
Kris Kurzawa <kurzawa1@yahoo.com>
2024-03-02 13:26:34 -0500

    added copyright sybol

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,10 @@
 import './Footer.css'
 
 const Footer = () => {
+    const today = new Date();
     return (
         <footer className='footer w-full h-28 bg-[#971522] text-[whitesmoke] text-center flex flex-col justify-center items-center text-xs lg:text-lg'>
-            <p>Copyright @ 2024 Kriskurzawaguitar - All Rights Reserved</p>
+            <p>Copyright &copy; {today.getFullYear()} Kriskurzawaguitar - All Rights Reserved</p>
             <button onClick={() => setTimeout(() => window.open('https://immense-everglades-74554-f9bad473782a.herokuapp.com/', '_blank'), 500)} className='webdev-link'>Site Designed and Built by Kris Kurzawa</button>
         </footer>
     )
