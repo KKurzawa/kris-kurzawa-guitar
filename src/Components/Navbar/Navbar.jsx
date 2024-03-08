d83a736f6752cd8e8bfcef125ce1a04ec8244c22
Kris Kurzawa <kurzawa1@yahoo.com>
2024-03-08 00:16:58 -0500

    message

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import './Navbar.css';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { navLinks } from '../../assets/data/navLinks';
 import { icons } from '../../assets/data/icons';
@@ -9,8 +9,17 @@ import { FaBars, FaTimes } from 'react-icons/fa';
 import KrisKurzawaGuitar2 from '../../assets/images/KrisKurzawaGuitar2.jpg'
 // import NavLogo from '../../assets/images/navLogo.png'
 
+import { AnimatePresence, motion } from "framer-motion";
+import { Squash as Hamburger } from "hamburger-react";
+import { useClickAway } from "react-use";
+
 const Navbar = () => {
-  const [open, setOpen] = useState(false);
+  // const [open, setOpen] = useState(false);
+
+  const [isOpen, setOpen] = useState(false);
+  // const ref = useRef(null);
+
+  // useClickAway(ref, () => setOpen(false));
 
   const navigate = useNavigate();
 
@@ -95,7 +104,7 @@ const Navbar = () => {
         </nav>
       </main>
       {/* small and medium */}
-      <main className='navbar flex flex-col lg:hidden text-[#f5f5f5]'>
+      {/* <main className='navbar flex flex-col lg:hidden text-[#f5f5f5]'>
         <article className='flex flex-row justify-between items-center bg-[#971522]'>
           <button type='button' onClick={handleMenu} className='bars px-2 py-2'>
             <span className='sr-only'>Open Main Menu</span>
@@ -131,6 +140,79 @@ const Navbar = () => {
             </i>
           </article>
         ) : null}
+      </main> */}
+      {/* hamburger experiment */}
+      <main className='navbar flex flex-col lg:hidden text-[#f5f5f5] text-center'>
+        <div className='flex justify-between items-center bg-[#97161D]'>
+          <div className='hamburger-btn'>
+            <Hamburger toggled={isOpen} size={20} toggle={setOpen} />
+          </div>
+          <h1 className=' text-4xl font-bold'>Kris Kurzawa</h1>
+          <div className='hamburger-btn'>
+            <Hamburger toggled={isOpen} size={20} toggle={setOpen} />
+          </div>
+        </div>
+        <div>
+          <LazyLoadImage src={KrisKurzawaGuitar2} className='navGuitarPic w-full' />
+          <h2 className='text-md text-center py-1 bg-[#97161D]'>Guitarist/Compsoser/Music Educator</h2>
+        </div>
+        <AnimatePresence>
+          {isOpen && (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              transition={{ duration: 1 }}
+              className="relative left-0 right-0 p-5 pt-0 border-b-[1px] border-[#eed29bb1] flex justify-center"
+            >
+              <ul className="text-center">
+
+
+                <motion.li
+                  initial={{ scale: 0, opacity: 0 }}
+                  animate={{ scale: 1, opacity: 1 }}
+                  transition={{
+                    type: "spring",
+                    stiffness: 260,
+                    damping: 20,
+                    delay: 0.5 + navLinks.id / 10,
+                  }}
+                  // key={route.title}
+                  className="w-full p-[0.08rem] rounded-xl "
+                >
+                  {/* <a
+                        onClick={() => setOpen((prev) => !prev)}
+                        className={
+                          "flex items-center justify-center w-full rounded-xl"
+                        }
+                        href={route.link}
+                      >
+                        <span className="flex text-lg text-white justify-center text-center">{route.title}</span>
+                      </a> */}
+                  <ol className='text-5xl pb-3 pt-2 flex flex-col items-center'>
+                    <button onClick={goToHome} className='small-nav-link'>{navLinks[0].title}</button>
+                    <button onClick={goToMusic} className='small-nav-link'>{navLinks[1].title}</button>
+                    <button onClick={goToVideo} className='small-nav-link'>{navLinks[2].title}</button>
+                    <button onClick={goToBio} className='small-nav-link'>{navLinks[3].title}</button>
+                    <button onClick={goToShows} className='small-nav-link'>{navLinks[4].title}</button>
+                    <button onClick={goToContact} className='small-nav-link'>{navLinks[5].title}</button>
+                  </ol>
+                  <i className='flex justify-center'>
+                    {icons.map((icon) => (
+                      <button className='small-icons px-2 text-4xl' key={icon.index} onClick={() => setTimeout(() => window.open(icon.link, '_blank'), 500)}>
+                        {icon.symbol}
+                      </button>
+                    ))}
+                  </i>
+                </motion.li>
+
+
+              </ul>
+            </motion.div>
+          )}
+        </AnimatePresence>
+
+
       </main>
     </>
   )
--- src/assets/data/navLinks.jsx
+++ src/assets/data/navLinks.jsx
@@ -1,25 +1,31 @@
 export const navLinks = [
     {
+        id: 1,
         title: 'Home',
         link: '/',
     },
     {
+        id: 2,
         title: 'Music',
         link: '/Music',
     },
     {
+        id: 3,
         title: 'Videos',
         link: '/Videos',
     },
     {
+        id: 4,
         title: 'Bio',
         link: '/Bio',
     },
     {
+        id: 5,
         title: 'Shows',
         link: '/Shows',
     },
     {
+        id: 6,
         title: 'Contact',
         link: '/Contact',
     },
