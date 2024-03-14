988f7faf1c6430fa2a52a9e4d9bac6d107f30267
Kris Kurzawa <kurzawa1@yahoo.com>
2024-03-14 12:51:13 -0400

    styled landing

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -8,7 +8,7 @@ const Landing = () => {
             <main className='landing-container hidden h-screen md:flex justify-center'>
                 <article className='fade-out flex justify-center items-center w-full h-screen'>
                     <img src={Logo} alt="logo" className='logo' />
-                    <h1 className='logo-text text-7xl mb-4 pb-2 text-[whitesmoke]'>Kris Kurzawa</h1>
+                    <h1 className='logo-text text-7xl mb-4 pb-2 text-[whitesmoke]'>Kris <span className='text-[#eed29bb1]'>Kurzawa</span></h1>
                     <h2 className='logo-text text-4xl mb-2 pb-2 text-[whitesmoke]'>Guitarist / Composer / Music Educator</h2>
                 </article>
             </main>
@@ -16,8 +16,8 @@ const Landing = () => {
             <main className='landing-container h-screen flex justify-center md:hidden'>
                 <article className='sm-fade-out flex justify-center items-center h-screen'>
                     <img src={Logo} alt="logo" className='sm-logo' />
-                    <h1 className='sm-logo-text text-2xl mb-2 text-[whitesmoke]'>Kris Kurzawa</h1>
-                    <h2 className='sm-logo-text text-xl mb-2 text-[whitesmoke]'>Guitarist/Composer/Music Educator</h2>
+                    <h1 className='sm-logo-text text-2xl mb-2 text-[whitesmoke]'>Kris <span className='text-[#eed29bb1]'>Kurzawa</span></h1>
+                    <h2 className='sm-logo-text text-md mb-2 text-[whitesmoke]'>Guitarist/Composer/Music Educator</h2>
                 </article>
             </main>
         </>
