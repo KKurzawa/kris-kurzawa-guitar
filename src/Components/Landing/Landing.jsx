00cb342d829ec2438544073b99b01f06f55a3c75
Kris Kurzawa <kurzawa1@yahoo.com>
2024-03-14 16:22:40 -0400

    added line animation to landing

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -8,16 +8,30 @@ const Landing = () => {
             <main className='landing-container hidden h-screen md:flex justify-center'>
                 <article className='fade-out flex justify-center items-center w-full h-screen'>
                     <img src={Logo} alt="logo" className='logo' />
-                    <h1 className='logo-text text-7xl mb-4 pb-2 text-[whitesmoke]'>Kris <span className='text-[#eed29bb1]'>Kurzawa</span></h1>
-                    <h2 className='logo-text text-4xl mb-2 pb-2 text-[whitesmoke]'>Guitarist / Composer / Music Educator</h2>
+                    <article className='flex flex-col items-center'>
+                        <h1 className='logo-text text-7xl pb-1 text-[whitesmoke]'>Kris <span className='text-[#eed29bb1]'>Kurzawa</span></h1>
+                        <hr className='line mb-4' />
+                    </article>
+                    <article className='flex flex-col items-center'>
+                        <h2 className='logo-text text-4xl pb-2 text-[whitesmoke]'>Guitarist / Composer / Music Educator</h2>
+                        <hr className='line mb-4' />
+                    </article>
+
                 </article>
             </main>
             {/* sm  */}
             <main className='landing-container h-screen flex justify-center md:hidden'>
                 <article className='sm-fade-out flex justify-center items-center h-screen'>
                     <img src={Logo} alt="logo" className='sm-logo' />
-                    <h1 className='sm-logo-text text-2xl mb-2 text-[whitesmoke]'>Kris <span className='text-[#eed29bb1]'>Kurzawa</span></h1>
-                    <h2 className='sm-logo-text text-md mb-2 text-[whitesmoke]'>Guitarist/Composer/Music Educator</h2>
+                    <article className='flex flex-col items-center'>
+                        <h1 className='sm-logo-text text-2xl  text-[whitesmoke]'>Kris <span className='text-[#eed29bb1]'>Kurzawa</span></h1>
+                        <hr className='sm-line mb-4' />
+                    </article>
+                    <article className='flex flex-col items-center'>
+                        <h2 className='sm-logo-text text-md mb-[.2rem] text-[whitesmoke]'>Guitarist/Composer/Music Educator</h2>
+                        <hr className='sm-line mb-4' />
+                    </article>
+
                 </article>
             </main>
         </>
