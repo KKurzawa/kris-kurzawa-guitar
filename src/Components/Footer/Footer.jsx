8cf8ae20203781b9062d9af4f14af48da599e614
Kris Kurzawa <kurzawa1@yahoo.com>
2024-03-25 16:07:24 -0400

    changed dev link

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className='footer w-full h-28 bg-[#971522] text-[whitesmoke] text-center flex flex-col justify-center items-center text-xs lg:text-lg'>
             <p>Copyright &copy; {today.getFullYear()} Kriskurzawaguitar - All Rights Reserved</p>
-            <button onClick={() => setTimeout(() => window.open('https://immense-everglades-74554-f9bad473782a.herokuapp.com/', '_blank'), 500)} className='webdev-link'>Site Designed and Built by Kris Kurzawa</button>
+            <button onClick={() => setTimeout(() => window.open('https://kris-dev-portfolio.vercel.app/', '_blank'), 500)} className='webdev-link'>Site Designed and Built by Kris Kurzawa</button>
         </footer>
     )
 }
