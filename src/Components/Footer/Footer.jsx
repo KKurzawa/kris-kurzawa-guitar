90e4f713c836ec516446ec1c570b40c637cc3b66
Kris Kurzawa <kurzawa1@yahoo.com>
2024-04-05 18:13:07 -0400

    changed dev link

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <footer className='footer flex flex-col justify-center items-center text-center w-full h-28 text-xs lg:text-lg'>
             <p>Copyright &copy; {today.getFullYear()} Kriskurzawaguitar - All Rights Reserved</p>
-            <button onClick={() => setTimeout(() => window.open('https://kris-dev-portfolio.vercel.app/', '_blank'), 500)} className='webdev-link'>Site Designed and Built by Kris Kurzawa</button>
+            <button onClick={() => setTimeout(() => window.open('https://www.kriskurzawadev.com/', '_blank'), 500)} className='webdev-link'>Site Designed and Built by Kris Kurzawa</button>
         </footer>
     )
 }
