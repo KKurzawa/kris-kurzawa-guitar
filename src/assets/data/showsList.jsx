6bbda357501b94d19351f4b8efe4395d5d765637
Kris Kurzawa <kurzawa1@yahoo.com>
2024-04-19 13:08:53 -0400

    updated shows

--- src/assets/data/showsList.jsx
+++ src/assets/data/showsList.jsx
@@ -1,26 +1,10 @@
 export const ShowsList = [
     {
-        date: 'April 6 2024',
-        time: '7pm',
-        band: 'Caravan Gypsy',
-        venue: "Vino and Vibes",
-        venueLink: "https://vinoandvibesbar.com/home",
-        ticketLink: 1
-    },
-    {
-        date: 'April 11 2024',
-        time: '7pm',
-        band: 'Caravan Gypsy',
-        venue: "Cata Vino",
-        venueLink: "https://laferiadetroit.com/",
-        ticketLink: 1
-    },
-    {
-        date: 'April 18 2024',
-        time: '6:30pm',
-        band: 'The Kris Kurzawa Trio',
-        venue: "Daxton Hotel",
-        venueLink: "https://daxtonhotel.com/",
+        date: 'April 25 2024',
+        time: '6pm',
+        band: 'The Thom Monks Trio',
+        venue: "Sappori",
+        venueLink: "https://www.google.com/search?client=safari&rls=en&q=sappori&ie=UTF-8&oe=UTF-8&dlnr=1&sei=PdQhZtWRHbO1wN4P7sGn2AQ",
         ticketLink: 1
     },
     {
@@ -29,7 +13,7 @@ export const ShowsList = [
         band: 'The Kris Kurzawa Group Plays Steely Dan',
         venue: 'The Cadieux Cafe',
         venueLink: 'https://www.cadieuxcafe.com',
-        ticketLink: false
+        ticketLink: 'https://www.eventbrite.com/e/the-music-of-steely-dan-with-the-kris-kurzawa-group-and-michael-king-tickets-880491763567?aff=oddtdtcreator&fbclid=IwZXh0bgNhZW0CMTAAAR1rw-AKJA5TDpX4Da8SSnh2aBYMWHspV_8asqP4uBMeXvvWzitzK7O4JZs_aem_AQDuem7EvoQdxzLv7f6OABknLapJLxgu3pPN9R2lBfF68n7PgbTMJOJ_J8TAG5EaWu8lUN_LDr2gn8YhQ1ycM1Nz'
     },
     {
         date: 'April 29 2024',
@@ -48,19 +32,19 @@ export const ShowsList = [
         ticketLink: 1
     },
     {
-        date: 'May 11 2024',
-        time: 'TBD',
-        band: 'Barbara Payton and the Instigators',
-        venue: "Boardwalk Theatre",
-        venueLink: "https://thumbcoasttheaters.com/boardwalk-theatre",
-        ticketLink: false
+        date: 'May 16 2024',
+        time: '7pm',
+        band: 'Caravan Gypsy',
+        venue: "Cata Vino",
+        venueLink: "https://laferiadetroit.com/",
+        ticketLink: 1
     },
     {
         date: 'May 25 2024',
         time: '7pm',
         band: 'The Kris Kurzawa Group featuring Barbara Payton plays Led Zeppelin',
         venue: "The Cadieux Cafe",
-        venueLink: "https://www.cadieuxcafe.com",
+        venueLink: "https://www.eventbrite.com/e/cadieux-cafe-presents-barbara-paytonkris-kurzawas-led-zeppelin-tribute-tickets-851746525787?aff=oddtdtcreator&fbclid=IwZXh0bgNhZW0CMTAAAR0lErMGcW6Ty2IzNSMr3nQN7lC-kZkTpZ-vwPANqzED1y5kMC23tD_P_dA_aem_Aei8yv1-7dZboMwUyloIv5H26ddTb_Dgvxeqp0-C49j0P3lo4IPhSB9VZxJLlpJltuiWBi5w53sfZtkLKWBzRF-S",
         ticketLink: false
     },
     {
@@ -88,11 +72,131 @@ export const ShowsList = [
         ticketLink: false
     },
     {
+        date: 'June 8 2024',
+        time: '11am',
+        band: 'Caravan Gypsy',
+        venue: 'The Sardine Room',
+        venueLink: 'https://www.thesardineroom.com',
+        ticketLink: 1
+    },
+    {
+        date: 'June 13 2024',
+        time: '7pm',
+        band: 'Caravan Gypsy',
+        venue: "Cata Vino",
+        venueLink: "https://laferiadetroit.com/",
+        ticketLink: 1
+    },
+    {
         date: 'June 14 2024',
-        time: '8:30pm',
-        band: 'The Kris Kurzawa Group Plays Steely Dan',
+        time: '7:30pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
         venue: 'The Cadieux Cafe',
         venueLink: 'https://www.cadieuxcafe.com',
+        ticketLink: false
+    },
+    {
+        date: 'June 22 2024',
+        time: '4:30pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        venue: 'Brighton, MI',
+        venueLink: 'N/A',
+        ticketLink: false
+    },
+    {
+        date: 'July 12 2024',
+        time: '7:30pm',
+        band: 'The Kris Kurzawa Group featuring Barbara Payton plays Led Zeppelin',
+        venue: "The Cadieux Cafe",
+        venueLink: "https://www.cadieuxcafe.com",
+        ticketLink: false
+    },
+    {
+        date: 'July 20 2024',
+        time: '7pm',
+        band: 'Barbara Payton and the Instigators',
+        venue: "Port Huron",
+        venueLink: "n/a",
+        ticketLink: false
+    },
+    {
+        date: 'July 25 2024',
+        time: '7pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        venue: "Milford, MI",
+        venueLink: "n/a",
+        ticketLink: false
+    },
+    {
+        date: 'August 1 2024',
+        time: '7pm',
+        band: 'Caravan Gypsy',
+        venue: "Cata Vino",
+        venueLink: "https://laferiadetroit.com/",
+        ticketLink: 1
+    },
+    {
+        date: 'August 8 2024',
+        time: '7pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        venue: "Royal Oak, MI",
+        venueLink: "n/a",
+        ticketLink: 1
+    },
+    {
+        date: 'August 16 2024',
+        time: '7:30pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        venue: 'The Cadieux Cafe',
+        venueLink: 'https://www.cadieuxcafe.com',
+        ticketLink: false
+    },
+    {
+        date: 'August 17 2024',
+        time: '7pm',
+        band: 'The Kris Kurzawa Group Featuring Barbara Payton Play Led Zeppelin',
+        venue: 'Wildwood Ampetheater',
+        venueLink: 'https://www.orionparks.com/facilities/parks_and_trails/wildwood.php',
+        ticketLink: false
+    },
+    {
+        date: 'September 8 2024',
+        time: '2pm',
+        band: 'Barbara Payton and the Instigators',
+        venue: "Lighthouse Porch Concert",
+        venueLink: "n/a",
+        ticketLink: 1
+    },
+    {
+        date: 'September 11 2024',
+        time: '7pm',
+        band: 'The Kris Kurzawa Group Featuring Barbara Payton Play Led Zeppelin',
+        venue: 'Brighton',
+        venueLink: '',
+        ticketLink: false
+    },
+    {
+        date: 'September 13 2024',
+        time: '7pm',
+        band: 'Caravan Gypsy',
+        venue: "Cliff Bells",
+        venueLink: "https://cliffbells.com",
+        ticketLink: 2
+    },
+    {
+        date: 'September 14 2024',
+        time: '7pm',
+        band: 'Caravan Gypsy',
+        venue: "Cliff Bells",
+        venueLink: "https://cliffbells.com",
+        ticketLink: 2
+    },
+    {
+        date: 'October 8 2024',
+        time: '7pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        venue: 'Wildwood Ampetheater',
+        venueLink: 'https://www.orionparks.com/facilities/parks_and_trails/wildwood.php',
         ticketLink: false
     },
     {
