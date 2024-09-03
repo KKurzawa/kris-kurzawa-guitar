7af663f2c6d7d43378edb0a1dbf3e7fd51817bae
Kris Kurzawa <kurzawa1@yahoo.com>
2024-09-03 14:27:31 -0400

    removed alt attribute in landing

--- src/Components/Landing/Landing.jsx
+++ src/Components/Landing/Landing.jsx
@@ -7,7 +7,7 @@ const Landing = () => {
             {/* md and lg  */}
             <main className='landing-container hidden h-screen md:flex justify-center'>
                 <article className='fade-out flex justify-center items-center w-full h-screen'>
-                    <img src={Logo} alt="logo" className='logo' />
+                    <img src={Logo} alt="" className='logo' />
                     <article className='flex flex-col items-center'>
                         <h1 className='kris-text pb-1 text-7xl'>Kris <span className='kurzawa-text'>Kurzawa</span></h1>
                         <hr className='line mb-4' />
@@ -22,7 +22,7 @@ const Landing = () => {
             {/* sm  */}
             <main className='landing-container h-screen flex justify-center md:hidden'>
                 <article className='sm-fade-out flex justify-center items-center h-screen'>
-                    <img src={Logo} alt="logo" className='sm-logo' />
+                    <img src={Logo} alt="" className='sm-logo' />
                     <article className='flex flex-col items-center'>
                         <h1 className='sm-kris-text text-2xl'>Kris <span className='sm-kurzawa-text'>Kurzawa</span></h1>
                         <hr className='sm-line mb-4' />
