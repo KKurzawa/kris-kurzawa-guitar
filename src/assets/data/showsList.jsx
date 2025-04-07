a9494dd37ba69a47dd81fb7c61cc8ab2c2995892
Kris Kurzawa <kurzawa1@yahoo.com>
2025-04-06 20:32:06 -0400

    updated shows

--- src/assets/data/showsList.jsx
+++ src/assets/data/showsList.jsx
@@ -1,233 +1,403 @@
 export const ShowsList = [
     {
-        date: 'October 24 2024',
+        date: 'April 9 2025',
+        time: '6:30pm',
+        band: 'The Kris Kurzawa Trio',
+        bandLink: false,
+        venue: "Chicane Plymouth",
+        city: 'Plymouth, MI',
+        venueLink: "https://www.chicane.com/",
+        ticketLink: 1
+    },
+    {
+        date: 'April 11 2025',
+        time: '5:30pm',
+        band: 'Caravan Gypsy',
+        bandLink: false,
+        venue: "Fiamma Grille",
+        city: 'Plymouth, MI',
+        venueLink: 'https://www.fiammagrillandbar.com',
+        ticketLink: 1
+    },
+    {
+        date: 'April 16 2025',
+        time: '6:30pm',
+        band: 'The Kris Kurzawa Trio',
+        bandLink: false,
+        venue: "Chicane Plymouth",
+        city: 'Plymouth, MI',
+        venueLink: "https://www.chicane.com/",
+        ticketLink: 1
+    },
+    {
+        date: 'April 17 2025',
         time: '7pm',
         band: 'Caravan Gypsy',
         bandLink: false,
         venue: "Cata Vino",
         city: 'Detroit, MI',
-        venueLink: "https://laferiadetroit.com/",
+        venueLink: 'https://laferiadetroit.com/bar-mercado/',
         ticketLink: 1
     },
     {
-        date: 'November 1 2024',
+        date: 'April 19 2025',
+        time: '8:30pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        bandLink: 'https://www.revivalbanddetroit.com/',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: 'https://www.eventbrite.com/e/revival-a-tribute-to-the-allman-brothers-band-tickets-1286692705519?aff=oddtdtcreator&fbclid=IwY2xjawJf0ShleHRuA2FlbQIxMAABHj4RCaYlw88awWNq58hEED-8wMPU7-TcuF8gzVHHd1BxNheZr7APYVA_mdvG_aem__jzIenhx8YNe-uXOsEdpEA'
+    },
+    {
+        date: 'May 2 2025',
+        time: '7:30pm',
+        band: 'The Kris Kurzawa Trio',
+        bandLink: false,
+        venue: "Chicane Plymouth",
+        city: 'Plymouth, MI',
+        venueLink: "https://www.chicane.com/",
+        ticketLink: 1
+    },
+    {
+        date: 'May 3 2025',
+        time: '7:30pm',
+        band: 'The Kris Kurzawa Trio',
+        bandLink: false,
+        venue: "Chicane Plymouth",
+        city: 'Plymouth, MI',
+        venueLink: "https://www.chicane.com/",
+        ticketLink: 1
+    },
+    {
+        date: 'May 8 2025',
+        time: '7pm',
+        band: 'Caravan Gypsy',
+        bandLink: false,
+        venue: "Cata Vino",
+        city: 'Detroit, MI',
+        venueLink: 'https://laferiadetroit.com/bar-mercado/',
+        ticketLink: 1
+    },
+    {
+        date: 'May 9',
+        time: '8:30pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        bandLink: 'https://www.revivalbanddetroit.com/',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'May 16',
+        time: '7:30pm',
+        band: 'Bob Seger Tribute',
+        bandLink: false,
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: 'https://www.eventbrite.com/e/an-evening-with-laura-creamer-and-barbara-payton-tickets-1304255987769?aff=oddtdtcreator&fbclid=IwY2xjawJf2mNleHRuA2FlbQIxMAABHivXuS9oig0saYA3trSkuRGwFiI0SnReKtUL8wm42BAmpIn-5sAciVSUehKw_aem_IG3gA1usJAjFaEadhSsxOw'
+    },
+    {
+        date: 'May 17',
+        time: '7pm',
+        band: 'Led Detroit',
+        bandLink: 'https://www.leddetroitband.com/',
+        venue: "Skyline Event Center",
+        city: 'Commins, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'May 23 2025',
+        time: '5:30pm',
+        band: 'Caravan Gypsy',
+        bandLink: false,
+        venue: "Fiamma Grille",
+        city: 'Plymouth, MI',
+        venueLink: 'https://www.fiammagrillandbar.com',
+        ticketLink: 1
+    },
+    {
+        date: 'May 24',
+        time: '7:30pm',
+        band: 'Led Detroit',
+        bandLink: 'https://www.leddetroitband.com/',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: 'https://www.eventbrite.com/e/cadieux-cafe-presents-barbara-paytonkris-kurzawas-led-zeppelin-tribute-tickets-1307177706709?aff=efbevent&fbclid=IwY2xjawJf0_ZleHRuA2FlbQIxMAABHlcxijqtcDa-YAWP03uhtuEaelbczUn7dbh0Cv6o-kXmRkGPz97wfIVy45WP_aem_vFLFK174_hSHFPWCawVczA'
+    },
+    {
+        date: 'June 6',
+        time: '6:30pm',
+        band: 'Barbara Payton',
+        bandLink: false,
+        venue: "Art on the River",
+        city: 'Port Huron, MI',
+        venueLink: 'https://www.artontheriverph.com/',
+        ticketLink: 1
+    },
+    {
+        date: 'June 13',
+        time: '8:30pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        bandLink: 'https://www.revivalbanddetroit.com/',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'June 19 2025',
+        time: '6pm',
+        band: 'The Kris Kurzawa Group',
+        bandLink: false,
+        venue: 'The Dirty Dog Jazz Cafe',
+        city: 'Grosse Point Farms, MI',
+        venueLink: 'https://dirtydogjazz.com/',
+        ticketLink: 2
+    },
+    {
+        date: 'June 20 2025',
+        time: '6pm',
+        band: 'The Kris Kurzawa Group',
+        bandLink: false,
+        venue: 'The Dirty Dog Jazz Cafe',
+        city: 'Grosse Point Farms, MI',
+        venueLink: 'https://dirtydogjazz.com/',
+        ticketLink: 2
+    },
+    {
+        date: 'June 21 2025',
+        time: '6pm',
+        band: 'The Kris Kurzawa Group',
+        bandLink: false,
+        venue: 'The Dirty Dog Jazz Cafe',
+        city: 'Grosse Point Farms, MI',
+        venueLink: 'https://dirtydogjazz.com/',
+        ticketLink: 2
+    },
+    {
+        date: 'June 28 2025',
+        time: '7:30pm',
+        band: 'Led Detroit',
+        bandLink: 'https://www.leddetroitband.com/',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'July 3',
+        time: '7pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        bandLink: 'https://www.revivalbanddetroit.com/',
+        venue: "Lafontaine Family Amphitheate",
+        city: 'Milford, MI',
+        venueLink: 'https://www.villageofmilford.org/discover/parks/Central-Park/discover/parks/lafontaine_family_amphitheater.php',
+        ticketLink: 1
+    },
+    {
+        date: 'July 17 2025',
+        time: '6pm',
+        band: 'Chain Reaction: The Joe Sample Tribute',
+        bandLink: false,
+        venue: 'The Dirty Dog Jazz Cafe',
+        city: 'Grosse Point Farms, MI',
+        venueLink: 'https://dirtydogjazz.com/',
+        ticketLink: 2
+    },
+    {
+        date: 'July 18 2025',
+        time: '6pm',
+        band: 'Chain Reaction: The Joe Sample Tribute',
+        bandLink: false,
+        venue: 'The Dirty Dog Jazz Cafe',
+        city: 'Grosse Point Farms, MI',
+        venueLink: 'https://dirtydogjazz.com/',
+        ticketLink: 2
+    },
+    {
+        date: 'July 19 2025',
+        time: '6pm',
+        band: 'Chain Reaction: The Joe Sample Tribute',
+        bandLink: false,
+        venue: 'The Dirty Dog Jazz Cafe',
+        city: 'Grosse Point Farms, MI',
+        venueLink: 'https://dirtydogjazz.com/',
+        ticketLink: 2
+    },
+    {
+        date: 'July 25 2025',
+        time: '7pm',
+        band: 'Led Detroit',
+        bandLink: 'https://www.leddetroitband.com/',
+        venue: "Stony Creek Metropark",
+        city: 'Shelby Twp.',
+        venueLink: 'https://www.metroparks.com/concerts/',
+        ticketLink: 1
+    },
+    {
+        date: 'July 26 2025',
+        time: '7:30pm',
+        band: 'Led Detroit',
+        bandLink: 'https://www.leddetroitband.com/',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: 2
+    },
+    {
+        date: 'August 2 2025',
+        time: '7:05pm',
+        band: 'Julian Ankley',
+        bandLink: false,
+        venue: "Dearborn Homecoming Festival",
+        city: 'Dearborn, MI',
+        venueLink: 'https://www.dearbornhomecoming.com/',
+        ticketLink: 1
+    },
+    {
+        date: 'August 8 2025',
+        time: '7:30pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        bandLink: 'https://www.revivalbanddetroit.com/',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'August 15 2025',
+        time: '7:30pm',
+        band: 'The Kris Kurzawa Group plays Santana',
+        bandLink: 'https://www.revivalbanddetroit.com/',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'August 23 2025',
+        time: '7:30pm',
+        band: 'Led Detroit',
+        bandLink: 'https://www.leddetroitband.com/',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'September 12 2025',
+        time: '7:30pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        bandLink: 'https://www.revivalbanddetroit.com/',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'September 27 2025',
+        time: '7:30pm',
+        band: 'Led Detroit',
+        bandLink: 'https://www.leddetroitband.com/',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'October 10 2025',
+        time: '7:30pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        bandLink: 'https://www.revivalbanddetroit.com/',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'October 17 2025',
+        time: '7:30pm',
+        band: 'The Kris Kurzawa Group plays Hendrix',
+        bandLink: false,
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'November 7 2025',
         time: '8:30pm',
         band: 'The Kris Kurzawa Group plays Steely Dan',
         bandLink: false,
         venue: "Cadieux Cafe",
         city: 'Detroit, MI',
         venueLink: 'https://www.cadieuxcafe.com/',
-        ticketLink: 2
+        ticketLink: false
     },
     {
-        date: 'November 4 2024',
-        time: '6pm',
-        band: 'The Kris Kurzawa Group',
-        bandLink: false,
-        venue: "Harrison's Speakeasy",
-        city: 'Harrison Twp, MI',
-        venueLink: 'http://harrisonsspeakeasy.com/',
-        ticketLink: 1
-    },
-    {
-        date: 'November 7 2024',
-        time: '6pm',
-        band: 'The Kris Kurzawa Group',
-        bandLink: false,
-        venue: 'The Dirty Dog Jazz Cafe',
-        city: 'Grosse Point Farms, MI',
-        venueLink: 'https://dirtydogjazz.com/',
-        ticketLink: 2
-    },
-    {
-        date: 'November 8 2024',
-        time: '6pm',
-        band: 'The Kris Kurzawa Group',
-        bandLink: false,
-        venue: 'The Dirty Dog Jazz Cafe',
-        city: 'Grosse Point Farms, MI',
-        venueLink: 'https://dirtydogjazz.com/',
-        ticketLink: 2
-    },
-    {
-        date: 'November 8 2024',
-        time: '8:30pm',
-        band: 'The Kris Kurzawa Group',
-        bandLink: false,
-        venue: 'The Dirty Dog Jazz Cafe',
-        city: 'Grosse Point Farms, MI',
-        venueLink: 'https://dirtydogjazz.com/',
-        ticketLink: 2
-    },
-    {
-        date: 'November 9 2024',
-        time: '6pm',
-        band: 'The Kris Kurzawa Group',
-        bandLink: false,
-        venue: 'The Dirty Dog Jazz Cafe',
-        city: 'Grosse Point Farms, MI',
-        venueLink: 'https://dirtydogjazz.com/',
-        ticketLink: 2
-    },
-    {
-        date: 'November 9 2024',
-        time: '8:30pm',
-        band: 'The Kris Kurzawa Group',
-        bandLink: false,
-        venue: 'The Dirty Dog Jazz Cafe',
-        city: 'Grosse Point Farms, MI',
-        venueLink: 'https://dirtydogjazz.com/',
-        ticketLink: 2
-    },
-    {
-        date: 'November 23 2024',
-        time: '8:30pm',
-        band: 'The Kris Kurzawa Group plays Santana',
-        bandLink: false,
-        venue: "Cadieux Cafe",
-        city: 'Detroit, MI',
-        venueLink: 'https://www.cadieuxcafe.com/',
-        ticketLink: 2
-    },
-    {
-        date: 'December 5 2024',
-        time: '6pm',
-        band: 'Chain Reaction: The Joe Sample Tribute',
-        bandLink: false,
-        venue: 'The Dirty Dog Jazz Cafe',
-        city: 'Grosse Point Farms, MI',
-        venueLink: 'https://dirtydogjazz.com/',
-        ticketLink: 2
-    },
-    {
-        date: 'December 6 2024',
-        time: '6pm',
-        band: 'Chain Reaction: The Joe Sample Tribute',
-        bandLink: false,
-        venue: 'The Dirty Dog Jazz Cafe',
-        city: 'Grosse Point Farms, MI',
-        venueLink: 'https://dirtydogjazz.com/',
-        ticketLink: 2
-    },
-    {
-        date: 'December 6  2024',
-        time: '8:30pm',
-        band: 'Chain Reaction: The Joe Sample Tribute',
-        bandLink: false,
-        venue: 'The Dirty Dog Jazz Cafe',
-        city: 'Grosse Point Farms, MI',
-        venueLink: 'https://dirtydogjazz.com/',
-        ticketLink: 2
-    },
-    {
-        date: 'December 7 2024',
-        time: '6pm',
-        band: 'Chain Reaction: The Joe Sample Tribute',
-        bandLink: false,
-        venue: 'The Dirty Dog Jazz Cafe',
-        city: 'Grosse Point Farms, MI',
-        venueLink: 'https://dirtydogjazz.com/',
-        ticketLink: 2
-    },
-    {
-        date: 'December 7 2024',
-        time: '8:30pm',
-        band: 'Chain Reaction: The Joe Sample Tribute',
-        bandLink: false,
-        venue: 'The Dirty Dog Jazz Cafe',
-        city: 'Grosse Point Farms, MI',
-        venueLink: 'https://dirtydogjazz.com/',
-        ticketLink: 2
-    },
-    {
-        date: 'January 3 2025',
-        time: '8:30pm',
-        band: 'The Abbey Road Project',
-        bandLink: 'https://www.abbeyroadproject.com/',
-        venue: "Cadieux Cafe",
-        city: 'Detroit, MI',
-        venueLink: 'https://www.cadieuxcafe.com/',
-        ticketLink: 2
-    },
-    {
-        date: 'January 4 2025',
-        time: '8:30pm',
-        band: 'Led Detroit',
-        bandLink: 'https://www.leddetroitband.com/',
-        venue: "Blind Owl",
-        city: 'Royal Oak, MI',
-        venueLink: 'https://www.blindowlrestaurant.com',
-        ticketLink: 2
-    },
-    {
-        date: 'January 11 2025',
+        date: 'November 14 2025',
         time: '8:30pm',
         band: 'The Kris Kurzawa Group plays Zappa',
         bandLink: false,
         venue: "Cadieux Cafe",
         city: 'Detroit, MI',
         venueLink: 'https://www.cadieuxcafe.com/',
-        ticketLink: 2
+        ticketLink: false
     },
     {
-        date: 'January 17 2025',
+        date: 'December 6 2025',
+        time: '8:30pm',
+        band: 'The Kris Kurzawa Group plays Santana',
+        bandLink: false,
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'January 30 2026',
         time: '8:30pm',
         band: 'Revival: A Tribute to the Allman Brothers',
         bandLink: 'https://www.revivalbanddetroit.com/',
         venue: "Cadieux Cafe",
         city: 'Detroit, MI',
         venueLink: 'https://www.cadieuxcafe.com/',
-        ticketLink: 2
+        ticketLink: false
     },
     {
-        date: 'January 18 2025',
+        date: 'January 31 2026',
         time: '8:30pm',
         band: 'Revival: A Tribute to the Allman Brothers',
         bandLink: 'https://www.revivalbanddetroit.com/',
         venue: "Cadieux Cafe",
         city: 'Detroit, MI',
         venueLink: 'https://www.cadieuxcafe.com/',
-        ticketLink: 2
+        ticketLink: false
     },
     {
-        date: 'Febuary 7 2025',
+        date: 'February 20 2026',
         time: '8:30pm',
         band: 'Led Detroit',
-        bandLink: false,
+        bandLink: 'https://www.leddetroitband.com/',
         venue: "Cadieux Cafe",
         city: 'Detroit, MI',
         venueLink: 'https://www.cadieuxcafe.com/',
-        ticketLink: 2
+        ticketLink: false
     },
     {
-        date: 'Febuary 8 2025',
+        date: 'February 21 2026',
         time: '8:30pm',
         band: 'Led Detroit',
-        bandLink: false,
+        bandLink: 'https://www.leddetroitband.com/',
         venue: "Cadieux Cafe",
         city: 'Detroit, MI',
         venueLink: 'https://www.cadieuxcafe.com/',
-        ticketLink: 2
-    },
-    {
-        date: 'March 22 2025',
-        time: '8:30pm',
-        band: 'The Kris Kurzawa Group plays Hendrix',
-        bandLink: false,
-        venue: "Cadieux Cafe",
-        city: 'Detroit, MI',
-        venueLink: 'https://www.cadieuxcafe.com/',
-        ticketLink: 2
-    },
-    {
-        date: 'April 4 2025',
-        time: '8:30pm',
-        band: 'The Kris Kurzawa Group plays Steely Dan',
-        bandLink: false,
-        venue: "Cadieux Cafe",
-        city: 'Detroit, MI',
-        venueLink: 'https://www.cadieuxcafe.com/',
-        ticketLink: 2
+        ticketLink: false
     },
 ]
 
