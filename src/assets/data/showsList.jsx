cae12088f301411e0ad385f695f0d90976a02c63
Kris Kurzawa <kurzawa1@yahoo.com>
2026-03-24 16:09:45 -0400

    updated shows

--- src/assets/data/showsList.jsx
+++ src/assets/data/showsList.jsx
@@ -90,10 +90,300 @@ export const ShowsList = [
         ticketLink: false
     },
     {
-        date: 'February 21 2026',
+        date: 'March 28 2026',
         time: '8:30pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        bandLink: 'https://www.revivalbanddetroit.com',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: 'https://www.eventbrite.com/e/revival-a-tribute-to-the-allman-brothers-band-tickets-1985106717630?aff=efbevent&fbclid=IwY2xjawQudoBleHRuA2FlbQIxMABicmlkETFTelNGWHJDQ0R5MW02S1gwc3J0YwZhcHBfaWQQMjIyMDM5MTc4ODIwMDg5MgABHnXDnxQrVaNQBPtRBW1bz_nuNGz4dpLf8r9621q1nd5LvJ2vUE8FZa3vjsiU_aem_CQFrjCOUr72dcw9GEmUPzQ'
+    },
+    {
+        date: 'April 3 2026',
+        time: '8:30pm',
+        band: 'The Kris Kurzawa Group Plays Hendrix',
+        bandLink: '',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: 'https://www.facebook.com/events/2298122123994857/?acontext=%7B%22event_action_history%22%3A[%7B%22mechanism%22%3A%22calendar_tab_event%22%2C%22surface%22%3A%22bookmark_calendar%22%7D]%2C%22ref_notif_type%22%3Anull%7D'
+    },
+    {
+        date: 'April 4 2026',
+        time: '8:30pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        bandLink: 'https://www.revivalbanddetroit.com',
+        venue: "Dryden Bar and Grille",
+        city: 'Dryden, MI',
+        venueLink: 'https://www.facebook.com/drydenbarandgrill',
+        ticketLink: 'https://www.facebook.com/events/4453293831567199/?acontext=%7B%22event_action_history%22%3A[%7B%22mechanism%22%3A%22calendar_tab_event%22%2C%22surface%22%3A%22bookmark_calendar%22%7D]%2C%22ref_notif_type%22%3Anull%7D'
+    },
+    {
+        date: 'April 11 2026',
+        time: '8:30pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        bandLink: 'https://www.revivalbanddetroit.com',
+        venue: "The Park Theater",
+        city: 'Holland, MI',
+        venueLink: 'https://parktheatreholland.org',
+        ticketLink: 'https://parktheatreholland.ludus.com/embed/select.php'
+    },
+    {
+        date: 'April 17',
+        time: '7pm',
+        band: 'The Kris Kurzawa Trio with Beth Stalker',
+        bandLink: false,
+        venue: "Chicane",
+        city: 'Plymouth, MI',
+        venueLink: 'https://www.chicane.com',
+        ticketLink: 1
+    },
+    {
+        date: 'April 18',
+        time: '7pm',
+        band: 'The Kris Kurzawa Trio with Beth Stalker',
+        bandLink: false,
+        venue: "Chicane",
+        city: 'Plymouth, MI',
+        venueLink: 'https://www.chicane.com',
+        ticketLink: 1
+    },
+    {
+        date: 'April 25 2026',
+        time: '8:30pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        bandLink: 'https://www.revivalbanddetroit.com',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'May 7 2026',
+        time: '6:30pm',
+        band: 'A Tribute to Joe Sample',
+        bandLink: false,
+        venue: "The Dirty Dog",
+        city: 'Grosse Point Farms, MI',
+        venueLink: 'https://dirtydogjazz.com/',
+        ticketLink: 2
+    },
+    {
+        date: 'May 8 2026',
+        time: '6:30pm',
+        band: 'A Tribute to Joe Sample',
+        bandLink: false,
+        venue: "The Dirty Dog",
+        city: 'Grosse Point Farms, MI',
+        venueLink: 'https://dirtydogjazz.com/',
+        ticketLink: 2
+    },
+    {
+        date: 'May 9 2026',
+        time: '6:30pm',
+        band: 'A Tribute to Joe Sample',
+        bandLink: false,
+        venue: "The Dirty Dog",
+        city: 'Grosse Point Farms, MI',
+        venueLink: 'https://dirtydogjazz.com/',
+        ticketLink: 2
+    },
+    {
+        date: 'May 15 2026',
+        time: '8:30pm',
+        band: 'The Kris Kurzawa Group Plays Frank Zappa',
+        bandLink: false,
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'May 22',
+        time: '7pm',
+        band: 'The Kris Kurzawa Trio with Beth Stalker',
+        bandLink: false,
+        venue: "Chicane",
+        city: 'Plymouth, MI',
+        venueLink: 'https://www.chicane.com',
+        ticketLink: 1
+    },
+    {
+        date: 'May 23',
+        time: '7pm',
+        band: 'The Kris Kurzawa Trio with Beth Stalker',
+        bandLink: false,
+        venue: "Chicane",
+        city: 'Plymouth, MI',
+        venueLink: 'https://www.chicane.com',
+        ticketLink: 1
+    },
+    {
+        date: 'June 8',
+        time: '6pm',
+        band: 'The Kris Kurzawa',
+        bandLink: false,
+        venue: "Harrison's Speakeasy",
+        city: 'Harrison Twp, MI',
+        venueLink: 'http://harrisonsspeakeasy.com/',
+        ticketLink: 1
+    },
+    {
+        date: 'June 12',
+        time: '7pm',
+        band: 'The Kris Kurzawa Trio with Beth Stalker',
+        bandLink: false,
+        venue: "Chicane",
+        city: 'Plymouth, MI',
+        venueLink: 'https://www.chicane.com',
+        ticketLink: 1
+    },
+    {
+        date: 'June 13',
+        time: '7pm',
+        band: 'The Kris Kurzawa Trio with Beth Stalker',
+        bandLink: false,
+        venue: "Chicane",
+        city: 'Plymouth, MI',
+        venueLink: 'https://www.chicane.com',
+        ticketLink: 1
+    },
+    {
+        date: 'July 10',
+        time: '8pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        bandLink: 'https://www.revivalbanddetroit.com',
+        venue: "Chateau Aeronautique",
+        city: 'Saline, MI',
+        venueLink: 'https://www.chateauaeronautiquewinery.com',
+        ticketLink: false
+    },
+    {
+        date: 'July 16',
+        time: '7pm',
         band: 'Led Detroit',
-        bandLink: 'https://www.leddetroitband.com/',
+        bandLink: 'https://www.leddetroitband.com',
+        venue: "Lafontaine Ampetheater",
+        city: 'Milford, MI',
+        venueLink: 'https://www.villageofmilford.org/discover/parks/Central-Park/discover/parks/lafontaine_family_amphitheater.php',
+        ticketLink: 1
+    },
+    {
+        date: 'July 17',
+        time: '7:30pm',
+        band: 'Countdown To Ecstasy',
+        bandLink: 'https://www.countdowntoecstasydetroit.com',
+        venue: "The Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'July 25 2026',
+        time: '7:30pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        bandLink: 'https://www.revivalbanddetroit.com',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'July 29 2026',
+        time: '7pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        bandLink: 'https://www.revivalbanddetroit.com',
+        venue: "Camp Dearborn",
+        city: 'Milford, MI',
+        venueLink: 'https://dearborn.gov/explore/parksandrec/campdearborn',
+        ticketLink: false
+    },
+    {
+        date: 'August 7 2026',
+        time: '5pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        bandLink: 'https://www.revivalbanddetroit.com',
+        venue: "Dexter, MI",
+        city: 'Dexter, MI',
+        venueLink: false,
+        ticketLink: false
+    },
+    {
+        date: 'August 15 2026',
+        time: '7:30pm',
+        band: 'Led Detroit',
+        bandLink: 'https://www.leddetroitband.com',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'August 29 2026',
+        time: '7:30pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        bandLink: 'https://www.revivalbanddetroit.com',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'September 2026',
+        time: '7:30pm',
+        band: 'The Kris Kurzawa Group Plays Zappa',
+        bandLink: false,
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'September 19 2026',
+        time: '7:30pm',
+        band: 'Led Detroit',
+        bandLink: 'https://www.leddetroitband.com',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'September 26 2026',
+        time: '7:30pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        bandLink: 'https://www.revivalbanddetroit.com',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'October 24 2026',
+        time: '7:30pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        bandLink: 'https://www.revivalbanddetroit.com',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'November 21 2026',
+        time: '8:30pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        bandLink: 'https://www.revivalbanddetroit.com',
+        venue: "Cadieux Cafe",
+        city: 'Detroit, MI',
+        venueLink: 'https://www.cadieuxcafe.com/',
+        ticketLink: false
+    },
+    {
+        date: 'December 2026',
+        time: '8:30pm',
+        band: 'Revival: A Tribute to the Allman Brothers',
+        bandLink: 'https://www.revivalbanddetroit.com',
         venue: "Cadieux Cafe",
         city: 'Detroit, MI',
         venueLink: 'https://www.cadieuxcafe.com/',
